530dd3fb0bb8dbfc2cc47b78a38958e0b47b8546
Ceddyzzz <voncedricsado@gmail.com>
2025-11-24 10:54:58 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
       <ul class="flex space-x-6">
         <li><a href="/PLMUN%20LMS/dashboard.php" class="hover:text-yellow-300 transition">Dashboard</a></li>
         <li><a href="/PLMUN%20LMS/announcement.php" class="hover:text-yellow-300 transition">Announcements</a></li>
-        <li><a href="/PLMUN%20LMS/chat_realtime.php" class="hover:text-yellow-300 transition">Chat</a></li>
+        <li><a href="/PLMUN%20LMS/chat.php" class="hover:text-yellow-300 transition">Chat</a></li>
         <li><a href="/PLMUN%20LMS/assignment.php" class="hover:text-yellow-300 transition">Assignments</a></li>
         <li><a href="/PLMUN%20LMS/calendar.php" class="hover:text-yellow-300 transition">Calendar</a></li>
         <li><a href="/PLMUN%20LMS/e-books.php" class="hover:text-yellow-300 transition">E-Books</a></li>
