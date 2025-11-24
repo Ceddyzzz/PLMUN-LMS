d1a3e9c2bd19063f5a2aabfc8b80769beaf237f5
Ceddyzzz <voncedricsado@gmail.com>
2025-11-24 10:54:47 +0800

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,12 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "plmun_lms";
+$username = "root";  // or your MySQL username
+$password = "";      // or your MySQL password
+$dbname = "plmun lms";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-  die("Database connection failed: " . $conn->connect_error);
+    die(json_encode(['error' => 'Database connection failed']));
 }
 ?>
